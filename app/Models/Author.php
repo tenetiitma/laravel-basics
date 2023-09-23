b3da97c37a4dffb50c5382ef22537a44c9464186
ralf <ralf@veebisepad.ee>
2023-09-23 10:01:39 +0300

    Add book & author relations

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Author extends Model
 {
     use HasFactory;
 
+    public function books(): BelongsToMany
+    {
+        return $this->belongsToMany(Book::class, 'book_authors');
+    }
 }
