777f088c816990a6b4847a16c58f97415d2b245e
ralf <ralf@veebisepad.ee>
2023-10-13 11:43:47 +0300

    3rd

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    public function book(): BelongsTo
+    {
+        return $this->BelongsTo(Book::class);
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->BelongsTo(Client::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Models\Author;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function(){
-    //siia kirjuta mida iganes
+Route::get('/test-1', function(){
 
+    return 'tere';
     // return Author::first()->books;
     // return Author::with('books')->first();
     // dd()
 });
+
+// Route::get('/authors', [AuthorController::class, 'index']);
+
+Route::resource('authors', AuthorController::class);
+Route::resource('clients', ClientController::class);
+
