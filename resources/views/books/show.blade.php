81096b4e1bad4981102e4a4a1f76c7f4f67ea8cb
Tene Tiitma <tenetiitma@gmail.com>
2023-11-02 17:17:21 +0200

    final show

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -8,24 +8,24 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 mx-auto w-max">
-                    <p class="flex flex-col m-2">
+                <div class="p-6 text-gray-900">
+                    <p>
                         <span>Title:</span>
                         <div>{{ $book->title }}</div>
                     </p>
-                    <p class="flex flex-col m-2">
+                    <p>
                         <span>Pages:</span>
                         <div>{{ $book->pages }}</div>
                     </p>
-                    <p class="flex flex-col m-2">
+                    <p>
                         <span>Release date:</span>
                         <div>{{ $book->release_date }}</div>
                     </p>
-                    <p class="flex flex-col m-2">
+                    <p>
                         <span>Price:</span>
                         <div>{{ $book->price }}</div>
                     </p>
-                    <p class="flex flex-col m-2">
+                    <p>
                         <span>Authors:</span>
                         @foreach ($book->authors as $author)
                         <div>
